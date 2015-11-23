a8bf21d3c4f26e084061072d3e7582d945e3327c
ctapia <ctapia@UTP062457.grupoutp.net>
2015-11-23 16:42:09 -0500

    API Adobe Connect

--- app_adobe/controller/requestController.php
+++ app_adobe/controller/requestController.php
@@ -1,11 +1,58 @@
 <?php
-class requestController{
 
-	function getList(){
-		$room = $_POST['moderator'];
-		$begrecord = str_replace("/", "-", $_POST['textdesde']);
-		$endrecord = str_replace("/", "-", $_POST['texthasta']);
+class requestController {
 
-		include('app_adobe/views/salas_adobe.php');
-	}
+    function getList() {
+
+        set_time_limit(0);
+        //error_reporting(0);
+
+        $template = file_get_contents('app_adobe/views/salas_adobe.php');
+
+        $salas_regexp = "/<!--salas-->(.|\n){1,}<!--finsalas-->/";
+        $records_regexp = "/<!--grabacion-->(.|\n){1,}<!--fingrabacion-->/";
+        $user_regexp = "/<!--asistentes-->(.|\n){1,}<!--finasistentes-->/";
+
+        preg_match($salas_regexp, $template, $salasMatch);
+        preg_match($records_regexp, $template, $recordsMatch);
+        preg_match($user_regexp, $template, $userMatch);
+
+        require("common/libs/AdobeConnectClient.class.php");
+        require('common/tools/library.php');
+
+        $room = $_POST['moderator'];
+        $begrecord = str_replace("/", "-", $_POST['textdesde']);
+        $endrecord = str_replace("/", "-", $_POST['texthasta']);
+
+        $client = new AdobeConnectClient();
+
+        $client->setUser('moderador' . $room);
+        $client->setPassword('utp.moderador' . $room);
+
+        $client->makeAuth();
+        $mettings = $client->getAllMeetings();
+        $count = 0;
+
+        foreach ($mettings['my-mettings']['metting'] as $v) {
+            $count++;
+
+            if ($begrecord == "") {
+                $begrecord = $v['date-begin'];
+            }
+
+            if ($endrecord == "") {
+                $endrecord = date('Y-m-d');
+            }
+
+            $stringTemp = array("{nrosala}", "{name}", "{link}");
+            $stringReal = array($count, $v['name'], $v['domain-name'] . $v['url-path']);
+
+            $replaceTemplate = str_replace($stringTemp, $stringReal, $salasMatch[0]);
+        }
+
+        print $replaceTemplate;
+
+        //include('app_adobe/views/salas_adobe.php');
+    }
+
 }
